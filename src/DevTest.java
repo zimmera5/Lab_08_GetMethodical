0b5fff16fa21da272298b2c45dda46462ef02969
Mark Owings <owingsmk@mail.uc.edu>
2023-04-02 10:17:23 -0400

    Second true commit

--- src/DevTest.java
+++ src/DevTest.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class DevTest {
     public static void main(String[] args) {
+
+    String myName = "";
+    Scanner in = new Scanner(System.in);
+
+    myName = SafeInput.getNonZeroLenString(in, "Give me a name");
+
+    System.out.println("Your Name Is "+ myName);
+
 
     }
 }
--- src/SafeInput.java
+++ src/SafeInput.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
-
+public class SafeInput {
 
 public static String getNonZeroLenString(Scanner pipe, String prompt)
 {
     String retString = "";  // Set this to zero length. Loop runs until it isn’t
     do
     {
-        System.out.print(“\n” +prompt + ": "); // show prompt add space
+        System.out.print("\n" +prompt + ": "); // show prompt add space
         retString = pipe.nextLine();
     }while(retString.length() == 0);
 
